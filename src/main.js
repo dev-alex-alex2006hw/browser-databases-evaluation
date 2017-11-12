784f6ec80fba29cb2413833da90bac8f6508c26e
Alex Davis <alex2006hw@gmail.com>
2017-11-12 14:23:01 -0800

    rsa-keypair

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-
+import makeKey from './generate';
 import { loginUser } from "./users";
 
 const newUUID = '11111111'
 const firebaseUUID = '22222222'
 const user = { usertokenID: newUUID, firebaseID: firebaseUUID }
 loginUser(user);
+makeKey();
